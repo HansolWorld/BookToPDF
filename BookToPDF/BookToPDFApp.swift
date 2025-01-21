49fd9625eb0cda2b51138557debda4dde64ece9b
HansolWorld <hs9269@gmail.com>
2025-01-21 17:22:49 +0900

    init

---
+++ BookToPDF/BookToPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookToPDFApp.swift
+//  BookToPDF
+//
+//  Created by Hansol Eom on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BookToPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
